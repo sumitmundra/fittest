677c30b64a9b30acb131567519707677757b6aa8
sumit mundra <sumitmundra@outlook.com>
2018-03-20 12:12:21 +0530

    test

--- git demo/git demo/ViewController.swift
+++ git demo/git demo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("this is test demo")
+        print("after merge")
     }
 
     override func didReceiveMemoryWarning() {
